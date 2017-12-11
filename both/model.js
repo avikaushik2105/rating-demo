2bc4112f043f27c61bde2b165b7933f8dff43c8e
Betha Ravi Kiran <betha.ravikiran@gmail.com>
2017-12-11 11:28:16 +0530

    The list for rating was supposed to be served by server, but since the Meteor.startUp function did not have Meteor.isServer check, due the the isomorphic nature of functions in Meteor Framework, the data was being inserted into Minimongo again.
    So, one possible solution was to add Meteor.isServer() checking while inserting records into the collection for setup.
    But, even better was to move such insertion to Server side and hence the insertion is now moved into main.js under server folder so that this
    function will not run on the client side any more.

--- both/model.js
+++ both/model.js
@@ -1,20 +1,4 @@
 Ratings = new Mongo.Collection('ratings');
 
-Meteor.startup(function() {
-    if (Ratings.find().count() === 0) {
-        var data_ratings = [
-            {"_id": "MLR38pMTWhiTvnone", name: "Ferrai", rating: 0},
-            {"_id": "2rfaybPiTogKPFLjr", name: "Lamborigini", rating: 0},
-            {"_id": "ncvDrskNZS8Lcwdc3", name: "Pagani", rating: 0},
-            {"_id": "YrEewmSkmpQrwTKHm", name: "Lotus", rating: 0},
-            {"_id": "czwvxQx6L8gGsT7RM", name: "Ford", rating: 0},
-            {"_id": "dzwvxQx6L8gGsT7RM", name: "Toyota", rating: 0},
-            {"_id": "ezwvxQx6L8gGsT7RM", name: "Nissan", rating: 0},
-            {"_id": "fzwvxQx6L8gGsT7RM", name: "Honda", rating: 0},
-            {"_id": "gzwvxQx6L8gGsT7RM", name: "Suzuki", rating: 0},
-        ]
-        _.each(data_ratings, function (rating) {
-            Ratings.insert(rating);
-        });
-    }
-});
+// Should have been inside Meteor.isServer() function.
+// even better move it to Server folder, hence moved into Main.js
--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function() {
+    if (Ratings.find().count() === 0) {
+        const data_ratings = [
+            {"_id": "MLR38pMTWhiTvnone", name: "Ferrai", rating: 0},
+            {"_id": "2rfaybPiTogKPFLjr", name: "Lamborigini", rating: 0},
+            {"_id": "ncvDrskNZS8Lcwdc3", name: "Pagani", rating: 0},
+            {"_id": "YrEewmSkmpQrwTKHm", name: "Lotus", rating: 0},
+            {"_id": "czwvxQx6L8gGsT7RM", name: "Ford", rating: 0},
+            {"_id": "dzwvxQx6L8gGsT7RM", name: "Toyota", rating: 0},
+            {"_id": "ezwvxQx6L8gGsT7RM", name: "Nissan", rating: 0},
+            {"_id": "fzwvxQx6L8gGsT7RM", name: "Honda", rating: 0},
+            {"_id": "gzwvxQx6L8gGsT7RM", name: "Suzuki", rating: 0},
+        ];
+        _.each(data_ratings, function (rating) {
+            Ratings.insert(rating);
+        });
+    }
 });
